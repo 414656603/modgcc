7c8a6ef8f63cb4bdc38dd951f3259b6c85857165
liguangwei <414656603@qq.com>
2025-12-29 15:06:50 +0800

    swith test1 p:50

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 import <iostream>;
 import <format>;
-import employee;
+// import employee;
 
 using namespace std;
 
 int main()
 {
-    //Create and populate an employee.
+/*     //Create and populate an employee.
     Employee anEmployee;
     anEmployee.firstInitial='J';
     anEmployee.lastInitial='D';
@@ -17,6 +17,22 @@ int main()
     cout << format("Number:{}",anEmployee.employeeNumber) << endl;
     cout << format("Salary:${}",anEmployee.salary) << endl;
     cin.get() ;
-    return 0;
-    //t
+    return 0; */
+  
+    enum class Mode { Default, Custom, Standard };
+    int value { 42 };
+    Mode mode {  Mode::Custom }; //此处没有use enum Mode; 因此初始化时需要加【Mode::】
+    switch (mode) {
+        using enum Mode;
+        case Custom:
+            value = 84;
+            cout << format("Custom mode: value={}\n", value);
+        case Standard:
+            value = 21;
+            cout << format("Standard mode: value={}\n", value);
+        case Default:
+            value = 42;
+            cout << format("Standard mode: value={}\n", value);
+        break;
+    }
 }
